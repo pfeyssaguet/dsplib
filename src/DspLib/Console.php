bf39c6776d86e4258466ff7a32d141d99d0a5460
Pierre Feyssaguet <pfeyssaguet@gmail.com>
2014-02-20 23:45:35 +0100

    Make phpcs happy for PSR2

--- src/DspLib/Console.php
+++ src/DspLib/Console.php
@@ -4,19 +4,19 @@ namespace DspLib;
 
 class Console
 {
-	public static function getUserInput($silent = false)
-	{
-		if ($silent) {
-			shell_exec('stty -echo');
-		}
+    public static function getUserInput($silent = false)
+    {
+        if ($silent) {
+            shell_exec('stty -echo');
+        }
 
-		$userInput = fgets(STDIN);
+        $userInput = fgets(STDIN);
 
-		if ($silent) {
-			shell_exec('stty echo');
-		}
+        if ($silent) {
+            shell_exec('stty echo');
+        }
 
-		return $userInput;
-	}
+        return $userInput;
+    }
 }
 
